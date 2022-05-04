e9f2e92401bc804db0942df6b966da5afada4ca1
Kim Black <kimblackaz@gmail.com>
2022-05-04 12:45:45 -0600

    Finished the Team 2 Activity. Add to cart works!
    Co-authored-by: Avie <avieeich@users.noreply.github.com>
    Co-authored-by: Moon Moon <moon18004@users.noreply.github.com>

---
+++ src/js/productDetails.js
@@ -0,0 +1,51 @@
+import {
+  setLocalStorage
+} from "./utils.js";
+
+import {
+    getLocalStorage
+  } from "./utils.js";
+
+let cart = [];
+
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+    this.cart = cart;
+  }
+  async init() {
+    this.product = await this.dataSource.findProductById(this.productId);
+    document.querySelector("main").innerHTML = this.renderProductDetails();
+    document.getElementById("addToCart")
+      .addEventListener("click", this.addToCart.bind(this));
+  }
+  addToCart() {
+    this.cart = getLocalStorage("so-cart");
+    if (this.cart != null) {
+      this.cart.push(this.product);
+    } else {
+      this.cart = [this.product];
+    }
+
+    setLocalStorage("so-cart", this.cart);
+  }
+  renderProductDetails() {
+    return `<section class="product-detail"> <h3>${this.product.Brand.Name}</h3>
+    <h2 class="divider">${this.product.NameWithoutBrand}</h2>
+    <img
+      class="divider"
+      src="${this.product.Image}"
+      alt="${this.product.NameWithoutBrand}"
+    />
+    <p class="product-card__price">$${this.product.FinalPrice}</p>
+    <p class="product__color">${this.product.Colors[0].ColorName}</p>
+    <p class="product__description">
+    ${this.product.DescriptionHtmlSimple}
+    </p>
+    <div class="product-detail__add">
+      <button id="addToCart" data-id="${this.product.Id}">Add to Cart</button>
+    </div></section>`;
+  }
+}

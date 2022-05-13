02110fe59e5de344987b61cba7d33f2b734544c0
Kim Black <kimblackaz@gmail.com>
2022-05-13 09:54:49 -0600

    started

--- src/js/cart.js
+++ src/js/cart.js
@@ -40,6 +40,7 @@ function renderCartItem(item) {
   <a href='#'>
     <h2 class='card__name'>${item.Name}</h2>
   </a>
+  <p class='cart-card__trash'> X </p>
   <p class='cart-card__color'>${item.Colors[0].ColorName}</p>
   <p class='cart-card__quantity'>qty: 1</p>
   <p class='cart-card__price'>$${item.FinalPrice}</p>

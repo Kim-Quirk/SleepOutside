776b60ef4ab7de44286c3c3fb47a374c9a93d28d
Kim Black <kimblackaz@gmail.com>
2022-05-27 11:37:03 -0600

    updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import Alert from "./alerts.js";
 import ProductData from "./productData.js";
 import ProductList from "./productList.js";
-import { loadHeaderFooter } from "./utils.js";
+import { loadHeaderFooter, getParam } from "./utils.js";
 
 loadHeaderFooter();
 
@@ -18,14 +18,25 @@ checkBackpack();
 
 const freshAlert = new Alert("message", "background", "color");
 
+const category = getParam("category");
+console.log(category);
 // first create an instance of our ProductData class.
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData(category);
 // then get the element we want the product list to render in
 const listElement = document.querySelector(".product-list");
 // then create an instance of our ProductList class and send it the correct information.
-const myList = new ProductList("tents", dataSource, listElement);
+const myList = new ProductList(category, dataSource, listElement);
 // finally call the init method to show our products
 myList.init();
+
+// // first create an instance of our ProductData class.
+// const dataSource = new ProductData("tents");
+// // then get the element we want the product list to render in
+// const listElement = document.querySelector(".product-list");
+// // then create an instance of our ProductList class and send it the correct information.
+// const myList = new ProductList("tents", dataSource, listElement);
+// // finally call the init method to show our products
+// myList.init();
 
 function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
